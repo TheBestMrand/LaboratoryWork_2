d980a4f2fb5dd583b03b42c94f682c29236ae404
TheBestMrand <thebestmrand@hotmail.com>
2022-10-19 05:17:25 +0300

    Initial commit

---
+++ FirstSite/scripts/image-switch-en.js
@@ -0,0 +1,18 @@
+function change_image()
+{
+    var image = document.getElementById('main-image');
+
+    if(document.getElementById('second-image').checked)
+    {
+        image.src = "../images/switch/second.jpg";
+    }
+    else if(document.getElementById('third-image').checked)
+    {
+        image.src = "../images/switch/third.jpg";
+    }
+    else
+    {
+        image.src = "../images/switch/first.jpg";
+    }
+
+}
